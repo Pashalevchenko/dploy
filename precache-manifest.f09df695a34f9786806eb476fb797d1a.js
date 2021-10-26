5d88c06cc6a9838c10b1a070f118cd787a915fb4
Pavel Levchenko <plevchenko@star.global>
2021-10-26 18:17:55 +0300

    Updates

---
+++ precache-manifest.f09df695a34f9786806eb476fb797d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835993e27cb6c541c58db75a91bf4f02",
+    "url": "/dploy/index.html"
+  },
+  {
+    "revision": "611216bd15a091a92922",
+    "url": "/dploy/static/css/main.988f1893.chunk.css"
+  },
+  {
+    "revision": "ca77627b6a30990c1a89",
+    "url": "/dploy/static/js/2.3c152af1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/dploy/static/js/2.3c152af1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611216bd15a091a92922",
+    "url": "/dploy/static/js/main.2df685d6.chunk.js"
+  },
+  {
+    "revision": "859613bba6e263618381",
+    "url": "/dploy/static/js/runtime-main.cc35ef10.js"
+  }
+]);
